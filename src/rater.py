9a077eb73c5f617d0f77bc5e3ae42fa63e40643e
MAJDOUB <abdelkarim.majdoub@capgemini.com>
2024-01-07 01:27:56 +0100

    manage and validate JSON files

---
+++ src/rater.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+##############################################################################
+This module is designed to manage and validate JSON files,
+specifically for airport data.
+##############################################################################
+"""
+
+import os
+
+class JsonAirport:
+    FORMAT = "json"
+    
+    def __init__(self, json_path):
+        self.json_path = json_path
+        self.validateJson()
+
+    def validateJson(self):
+        if not self.exists():
+            raise FileNotFoundError(f"JSON file not found: {self.json_path}")
+        elif not self.isCorrectFormat():
+            raise ValueError(f"File format must be {JsonAirport.FORMAT}")
+        
+    def __repr__(self):  
+        return (f"Filename = {self.basename()}\n"
+                f"Format = {JsonAirport.FORMAT}\n"
+                f"Directory = {self.dirname()}\n"
+                f"File exists = {self.exists()}\n")
+    
+    def exists(self):
+        return os.path.isfile(self.json_path)
+
+    def basename(self):
+        return os.path.basename(self.json_path)
+
+    def dirname(self):
+        return os.path.dirname(os.path.abspath(self.json_path))
+
+    def extension(self):
+        _, ext = os.path.splitext(self.json_path)
+        return ext.lstrip('.')
+
+    def isCorrectFormat(self):
+        return self.extension().lower() == JsonAirport.FORMAT
